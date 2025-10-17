440ecfaaf44655822ba68e3f05dda6d23e868d39
Licrovan <licrovanus@gmail.com>
2025-10-17 12:42:27 +0200

    UC19

--- Grocery.App/Views/ProductView.xaml.cs
+++ Grocery.App/Views/ProductView.xaml.cs
@@ -4,9 +4,18 @@ namespace Grocery.App.Views;
 
 public partial class ProductView : ContentPage
 {
-	public ProductView(ProductViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    private ProductViewModel _viewModel;
+
+    public ProductView(ProductViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.RefreshProducts();
+    }
 }
